b05e36ab5a4f438a64eef3e6eaff76f230f21250
camilobaquero <cami7ord@gmail.com>
2020-06-17 03:16:27 +0200

    Draw grid

--- widgets/src/main/java/com/example/widgets/charts/HeatingTimeChartView.kt
+++ widgets/src/main/java/com/example/widgets/charts/HeatingTimeChartView.kt
@@ -2,16 +2,21 @@ package com.example.widgets.charts
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.DashPathEffect
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.example.widgets.R
-import kotlin.math.absoluteValue
+import java.util.*
 
 class HeatingTimeChartView : View {
 
-    private var zeroLineY = 0.0f
+    private var borderTopY = 8f
+    private var borderLeftX = 8f
+    private var borderBottomY = 0f
+    private var borderRightX = 0f
+    private var lineSeparationY = 0f
+    private var lineSeparationX = 0f
     private var averageReturn = chartValues.average().toFloat()
 
     private val paint = Paint()
@@ -68,13 +73,13 @@ class HeatingTimeChartView : View {
             field = value
             invalidate()
         }
-    var zeroLineColor: Int = R.color.light_gray
+    var borderlineColor: Int = R.color.tado
         set(value) {
             field = value
             invalidate()
             requestLayout()
         }
-    var zeroLineWeight: Float = 2f
+    var borderlineWeight: Float = 8f
         set(value) {
             field = value
             invalidate()
@@ -89,11 +94,20 @@ class HeatingTimeChartView : View {
         initialize(attrs)
     }
 
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
         initialize(attrs)
     }
 
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(
+    constructor(
+        context: Context?,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(
         context,
         attrs,
         defStyleAttr,
@@ -107,14 +121,34 @@ class HeatingTimeChartView : View {
             val ta = context.obtainStyledAttributes(attrs, R.styleable.HeatingTimeChartView)
             try {
                 barWidth = ta.getDimension(R.styleable.HeatingTimeChartView_barWidth, barWidth)
-                positiveBarsColor = ta.getColor(R.styleable.HeatingTimeChartView_positiveBarsColor, positiveBarsColor)
-                negativeBarsColor = ta.getColor(R.styleable.HeatingTimeChartView_negativeBarsColor, negativeBarsColor)
-                averageLineColor = ta.getColor(R.styleable.HeatingTimeChartView_averageLineColor, averageLineColor)
-                zeroLineColor = ta.getColor(R.styleable.HeatingTimeChartView_zeroLineColor, zeroLineColor)
-                zeroLineWeight = ta.getDimension(R.styleable.HeatingTimeChartView_zeroLineWeight, zeroLineWeight)
-                averageLineWeight = ta.getDimension(R.styleable.HeatingTimeChartView_averageLineWeight, averageLineWeight)
-                averageIntervalOn = ta.getDimension(R.styleable.HeatingTimeChartView_averageIntervalOn, averageIntervalOn)
-                averageIntervalOff = ta.getDimension(R.styleable.HeatingTimeChartView_averageIntervalOff, averageIntervalOff)
+                positiveBarsColor = ta.getColor(
+                    R.styleable.HeatingTimeChartView_positiveBarsColor,
+                    positiveBarsColor
+                )
+                negativeBarsColor = ta.getColor(
+                    R.styleable.HeatingTimeChartView_negativeBarsColor,
+                    negativeBarsColor
+                )
+                averageLineColor =
+                    ta.getColor(R.styleable.HeatingTimeChartView_averageLineColor, averageLineColor)
+                borderlineColor =
+                    ta.getColor(R.styleable.HeatingTimeChartView_borderlineColor, borderlineColor)
+                borderlineWeight = ta.getDimension(
+                    R.styleable.HeatingTimeChartView_borderlineWeight,
+                    borderlineWeight
+                )
+                averageLineWeight = ta.getDimension(
+                    R.styleable.HeatingTimeChartView_averageLineWeight,
+                    averageLineWeight
+                )
+                averageIntervalOn = ta.getDimension(
+                    R.styleable.HeatingTimeChartView_averageIntervalOn,
+                    averageIntervalOn
+                )
+                averageIntervalOff = ta.getDimension(
+                    R.styleable.HeatingTimeChartView_averageIntervalOff,
+                    averageIntervalOff
+                )
             } finally {
                 ta.recycle()
             }
@@ -123,92 +157,106 @@ class HeatingTimeChartView : View {
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        zeroLineY = measuredHeight / 2.toFloat()
+        paint.color = ContextCompat.getColor(context, R.color.light_gray) // TODO use defaults
+        paint.textSize = 50f
+
+        //Order is important
+        borderBottomY = measuredHeight - borderTopY - paint.textSize * 2
+        lineSeparationY = borderBottomY / 3f
+        lineSeparationX = measuredWidth / chartValues.size.toFloat()
+        borderRightX = measuredWidth - paint.measureText("24h") - lineSeparationX
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        //TODO son 31 espacios, dividir por 31 el ancho
+
         if (items.isNotEmpty()) {
-            onDrawLines(canvas)
+
+            onDrawGrid(canvas)
+
+            /*onDrawLines(canvas)
 
             onDrawZeroLine(canvas)
 
-            onDrawAverageLine(canvas)
+            onDrawAverageLine(canvas)*/
         }
 
     }
 
-    private fun onDrawZeroLine(canvas: Canvas) {
+    private fun onDrawGrid(canvas: Canvas) {
 
-        paint.style = Paint.Style.STROKE
-        paint.strokeWidth = zeroLineWeight
-        paint.pathEffect = null
-        paint.color = zeroLineColor
-
-        canvas.drawLine(0f, zeroLineY, measuredWidth.toFloat(), zeroLineY, paint)
-
-    }
-
-    private fun onDrawAverageLine(canvas: Canvas) {
-
-        paint.style = Paint.Style.STROKE
-        paint.strokeWidth = averageLineWeight
-        paint.pathEffect = DashPathEffect(floatArrayOf(averageIntervalOn, averageIntervalOff), 0f)
-        paint.color = averageLineColor
-
-        canvas.drawLine(
-            0f, zeroLineY - zeroLineY * averageReturn * scalingFactor,
-            measuredWidth.toFloat(), zeroLineY - zeroLineY * averageReturn * scalingFactor, paint
-        )
-
-    }
-
-    private fun onDrawLines(canvas: Canvas) {
-        val interval: Float = measuredWidth / (items.size + 1).toFloat()
-        var step = 0f
-
-        paint.style = Paint.Style.STROKE
-        paint.strokeCap = Paint.Cap.ROUND
-        paint.strokeWidth = barWidth
         paint.pathEffect = null
 
-        for (value in items) {
-            step += interval
-            onDrawSingleLine(step, value, canvas)
+        onDrawYAxis(canvas)
+        onDrawXAxis(canvas)
+
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = borderlineWeight
+
+        canvas.drawRect(borderLeftX, borderTopY, borderRightX, borderBottomY, paint)
+        //canvas.drawLine(0f, zeroLineY, measuredWidth.toFloat(), zeroLineY, paint)
+
+    }
+
+    private fun onDrawYAxis(canvas: Canvas) {
+        var textY = 0f
+        val lastIndex = generateYaxisValues().lastIndex
+        val tempW = paint.strokeWidth
+        generateYaxisValues().forEachIndexed { index, i ->
+            textY = when (index) {
+                0 -> paint.textSize
+                lastIndex -> borderBottomY
+                else -> {
+                    paint.strokeWidth = borderlineWeight
+                    canvas.drawLine(
+                        borderRightX,
+                        lineSeparationY * index,
+                        borderLeftX,
+                        lineSeparationY * index,
+                        paint
+                    )
+                    (lineSeparationY * index) + (paint.textSize / 2)
+                }
+            }
+            paint.strokeWidth = tempW
+            canvas.drawText("${i}h", borderRightX + lineSeparationX, textY, paint)
         }
     }
 
-    private fun onDrawSingleLine(left: Float, value: Float, canvas: Canvas) {
-
-        val top: Float
-        val bottom: Float
-
-        canvas.save()
-
-        when {
-            value > 0 -> {
-                paint.color = positiveBarsColor
-                top = zeroLineY - (zeroLineY * value) * scalingFactor
-                bottom = zeroLineY
-                canvas.clipRect(0f, 0f, measuredWidth.toFloat(), zeroLineY)
-            }
-            value < 0 -> {
-                paint.color = negativeBarsColor
-                top = zeroLineY
-                bottom = zeroLineY + (zeroLineY * value).absoluteValue * scalingFactor
-                canvas.clipRect(0f, zeroLineY, measuredWidth.toFloat(), measuredHeight.toFloat())
-            }
-            else -> return
+    private fun onDrawXAxis(canvas: Canvas) {
+        var textX = lineSeparationX
+        val textY = measuredHeight.toFloat() - paint.textSize + (paint.textSize / 2)
+        generateXaxisValues(Calendar.getInstance().time).forEach {
+            canvas.drawText("$it", textX - (paint.measureText(it.toString()) / 2), textY, paint)
+            textX += lineSeparationX * 4f
         }
-
-        canvas.scale(1f, 1f - (paint.strokeWidth / measuredHeight), 0f, zeroLineY)
-        canvas.drawLine(left, top, left, bottom, paint)
-        canvas.restore()
-
     }
 
     companion object {
+
+        fun generateYaxisValues(): IntArray {
+            return yAxisValues
+        }
+
+        fun generateXaxisValues(date: Date): IntArray {
+            val results = IntArray(7)
+            val calendar = Calendar.getInstance()
+            calendar.time = date
+            for (i in 6.downTo(0)) {
+                results[i] = calendar.get(Calendar.DAY_OF_MONTH)
+                calendar.add(Calendar.DAY_OF_MONTH, -5)
+            }
+            return results
+        }
+
+        private val yAxisValues = intArrayOf(
+            24,
+            16,
+            8,
+            0
+        )
 
         val chartValues = listOf(
             19.53f,
@@ -221,9 +269,27 @@ class HeatingTimeChartView : View {
             12.78f,
             0f,
             13.41f,
-            29.6f,
-            11.39f
-        ).map { x -> x/100 }
+            19.53f,
+            26.38f,
+            8.99f,
+            3f,
+            13.62f,
+            3.53f,
+            23.45f,
+            12.78f,
+            0f,
+            13.41f,
+            19.53f,
+            26.38f,
+            8.99f,
+            3f,
+            13.62f,
+            3.53f,
+            23.45f,
+            12.78f,
+            0f,
+            13.41f
+        ).map { x -> x / 100 }
     }
 
 }
